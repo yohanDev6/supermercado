619a15443386e8c7db89aeb2fa63543511462674
yohan <yohan@yohan-debianpc>
2025-11-12 10:09:53 -0300

    Projeto inicial: estrutura Spring Boot e configuração do PostgreSQL

---
+++ src/main/java/br/com/yohandev6/SupermercadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.yohandev6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yohandev6/SupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yohandev6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
